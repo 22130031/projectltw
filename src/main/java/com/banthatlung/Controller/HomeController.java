0fcad7ae492f9b83999ebe86c6a5b0d4b72327da
BaoAnh524 <baoanh5114@gmail.com>
2025-01-07 00:58:12 +0700

    update Home

--- src/main/java/com/banthatlung/Controller/HomeController.java
+++ src/main/java/com/banthatlung/Controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.banthatlung.Controller;
 
+import com.banthatlung.Dao.model.Brand;
+import com.banthatlung.Dao.model.Product;
+import com.banthatlung.services.ProductService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,12 +10,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-@WebServlet(urlPatterns = {"/s"})
+import java.sql.SQLException;
+import java.util.List;
+
+
+@WebServlet(urlPatterns = {"/home"})
 public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductService productService = new ProductService();
         resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF-8");
-
+        List<Product> productList = null;
+        try {
+            productList = productService.getAll();
+            req.setAttribute("productList", productList);
+            req.getRequestDispatcher("/View/home.jsp").forward(req, resp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
